5b210687a61d083ce40e5efcc938ea767b8faa24
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-09 12:16:50 +0000

    Day nine solution

--- part-one.py
+++ part-one.py
@@ -3,12 +3,24 @@
 import os
 from time import perf_counter_ns
 
+def get_differences(values):
+    return [values[ix+1] - values[ix] for ix in range(len(values) - 1)]
+
+def get_next_value(line):
+    values = [int(value) for value in line.split(' ') if value != '']
+    difference_list = []
+    differences = get_differences(values)
+    while not all(map(lambda x: x == 0, differences)):
+        difference_list.append(differences)
+        differences = get_differences(differences)
+    return values[-1] + sum([differences[-1] for differences in difference_list])
+
 def answer(input_file):
     start = perf_counter_ns()
     with open(input_file, 'r') as input:
         data = input.read().split('\n')
 
-    answer = None
+    answer = sum([get_next_value(line) for line in data])
     end = perf_counter_ns()
 
     print(f'The answer is: {answer}')
--- part-two.py
+++ part-two.py
@@ -3,16 +3,35 @@
 import os
 from time import perf_counter_ns
 
+def get_differences(values):
+    return [values[ix+1] - values[ix] for ix in range(len(values) - 1)]
+
+def get_prev_value(line):
+    values = [int(value) for value in line.split(' ') if value != '']
+    
+    difference_list = []
+    differences = get_differences(values)
+    while not all(map(lambda x: x == 0, differences)):
+        difference_list.append(differences)
+        differences = get_differences(differences)
+
+    difference = 0
+    while len(difference_list) > 0:
+        differences = difference_list.pop()
+        difference = differences[0] - difference
+
+    return values[0] - difference
+
 def answer(input_file):
     start = perf_counter_ns()
-    with open(input_file, "r") as input:
+    with open(input_file, 'r') as input:
         data = input.read().split('\n')
 
-    answer = None
+    answer = sum([get_prev_value(line) for line in data])
     end = perf_counter_ns()
 
     print(f'The answer is: {answer}')
     print(f'{((end-start)/1000000):.2f} milliseconds')
 
-input_file = os.path.join(os.path.dirname(__file__), "input")
+input_file = os.path.join(os.path.dirname(__file__), 'input')
 answer(input_file)
